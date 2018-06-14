4c1eccb502257656f322381e59a0b65035fbedc8
Arthur Raposo <theraposofox@gmail.com>
2018-06-14 20:21:12 -0300

    New

--- MLP.py
+++ MLP.py
@@ -1,1 +1,14 @@
-print(list(range(10)+1))
+print(list(range(10)))
+for n in range(10):
+    print(n)
+    pass
+print("done")
+
+def avg(x,y):
+    print("first input is", x)
+    print("second input is", y)
+    a = (x+y)/ 2
+    print("average is", a)
+    return a
+
+print(avg(200,301))
